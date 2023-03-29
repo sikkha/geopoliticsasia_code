3446fdac10e28fc0a61ba9d8325230efe16bbfd9
Kan Yuenyong <kan@Kans-MacBook-Air.local>
2023-03-29 08:55:14 +0700

    revise new openai code to better secure privacy on OPENAI_API_KEY

--- code archives/sample_openai_api_chat.py
+++ code archives/sample_openai_api_chat.py
@@ -1,28 +1,46 @@
 import openai
 import re
 import os
+import requests
+import json
+
 
 # Initialize the OpenAI API key
-#openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Define a function to prompt the GPT-3 language model with user input
-def ask_gpt(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=1024,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        api_key="OPENAI_API_KEY"
-        #replace your own openai api in the api_key above, it's better to set it in the OpenAI API key system shell variable
-    )
-    message = response.choices[0].text.strip()
-    return re.sub('[^0-9a-zA-Z\n\.\?,!]+', ' ', message)
+def ask_gpt(prompt, engine_name):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai.api_key}",
+    }
+    data = {
+        "model": engine_name,
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        "temperature": 0.5,
+        #"max_tokens": 1024,
+        "max_tokens": 2048,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0
+    }
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(data))
+    response_json = response.json()
+    
+    if 'choices' in response_json and len(response_json['choices']) > 0:
+        message = response_json['choices'][0]['message']['content'].strip()
+    else:
+        message = "Sorry, I couldn't generate a response."
+
+    # return re.sub('[^0-9a-zA-Z\n\.\?,!]+', ' ', message)
+    return message  # Return the message without filtering characters
+
 
 # Define a function to handle the conversation
-def chat():
+def chat(engine_name):
     conversation_history = "Chatbot: Hi, I'm a chatbot. How can I help you?\n"
     print(conversation_history.strip())
 
@@ -34,10 +52,14 @@ def chat():
 
         conversation_history += f"You: {user_input}\n"
         conversation_history += "Chatbot: "
-        response = ask_gpt(conversation_history)
+        response = ask_gpt(conversation_history, engine_name)
+        # print("History: [", conversation_history, "]")
         print("Chatbot:", response)
 
         conversation_history += f"{response}\n"
 
-# Start the conversation
-chat()
+# Start the conversation with a specific engine
+engine_name = "gpt-4"  
+chat(engine_name)
+
+
